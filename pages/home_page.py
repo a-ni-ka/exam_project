3ec6ef048e16cf926f47dcf79c6c239a83418e90
ani <anika.barop@hotmail.de>
2025-01-28 16:06:20 +0100

    test for deployment

--- pages/home_page.py
+++ pages/home_page.py
@@ -27,12 +27,16 @@ else:
 st.title("Home")
 
 song_1 = search_songs("sou jogador mestre barrao")[0]
-song_2 = search_songs("boa voz berimbau viola")[0]
+song_2 = search_songs("manduca da praia coreba")[0]
 song_3 = search_songs("quem vem la mestre suassuna")[2]
-song_4 = search_songs("capoeira nago")[0]
+song_4 = search_songs("madeira boa abada")[0]
+song_5 = search_songs("berimbau viola boa voz")[0]
+song_6 = search_songs("perola negra barrao")[0]
+song_7 = search_songs("a mare a mare nago")[2]
+song_8 = search_songs("carolina soares marinheiro")[0]
 
 with st.container():
-    st.subheader("Top songs")
+    st.subheader("Top songs - Energetic")
     r1c1, r1c2, r1c3, r1c4 = st.columns(4)
     with r1c1:
         st.image(song_1["album_cover"], width=160)
@@ -60,10 +64,38 @@ with st.container():
             st.switch_page("pages/song_page.py")
 
 with st.container():
-    st.subheader("Popular Playlists")
-    r2c1, r2c2, r2c3, r2c4, r2c5 = st.columns(5)
-    r2c1.write("placeholder2")
-    r2c2.write("placeholder2")
-    r2c3.write("placeholder2")
-    r2c4.write("placeholder2")
-    r2c5.write("placeholder2")
+    st.subheader("Top Songs - Relaxed")
+    r2c1, r2c2, r2c3, r2c4 = st.columns(4)
+    with r2c1:
+        st.image(song_5["album_cover"], width=160)
+        button_5 = st.button(f"{song_5["title"]} by {song_5["artist"]}")
+        if button_5:
+            st.session_state.current_song = song_5
+            st.switch_page("pages/song_page.py")
+    with r2c2:
+        st.image(song_6["album_cover"], width=160)
+        button_6 = st.button(f"{song_6["title"]} by {song_6["artist"]}")
+        if button_6:
+            st.session_state.current_song = song_6
+            st.switch_page("pages/song_page.py")
+    with r2c3:
+        st.image(song_7["album_cover"], width=160)
+        button_7 = st.button(f"{song_7["title"]} by {song_7["artist"]}")
+        if button_7:
+            st.session_state.current_song = song_7
+            st.switch_page("pages/song_page.py")
+    with r2c4:
+        st.image(song_8["album_cover"], width=160)
+        button_8 = st.button(f"{song_8["title"]} by {song_8["artist"]}")
+        if button_8:
+            st.session_state.current_song = song_8
+            st.switch_page("pages/song_page.py")
+
+
+with st.container():
+    st.subheader("Top Songs - Modern")
+    r3c1, r3c2, r3c3, r3c4 = st.columns(4)
+    r3c1.write("placeholder2")
+    r3c2.write("placeholder2")
+    r3c3.write("placeholder2")
+    r3c4.write("placeholder2")
--- pages/learn_page.py
+++ pages/learn_page.py
@@ -23,16 +23,27 @@ with tab3:
     st.header("Capoeira History")
     with st.expander("The origins of Capoeira"):
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Rugendasroda.jpg/1200px-Rugendasroda.jpg",
-                 width=300,
+                 width=500,
                  caption="This file is licensed under the Creative Commons Attribution 3.0 Unported license.")
         st.write("""
-        'Capoeira developed as a result of more than three hundred years of slavery in Brazil. Enslaved Africans were taken by Portuguese colonists from various cultures in Africa. In Brazil, generations of enslaved African people shared the cultural customs, dances, rituals, and fighting techniques that would combine to become capoeira. Slaves used capoeira to fight to escape and resist capture, but concealed its combative purpose through music, song, and dance.
+        'Capoeira developed as a result of more than three hundred years of slavery in Brazil. 
+        Enslaved Africans were taken by Portuguese colonists from various cultures in Africa. 
+        In Brazil, generations of enslaved African people shared the cultural customs, dances, rituals, 
+        and fighting techniques that would combine to become capoeira. Slaves used capoeira to fight to escape 
+        and resist capture, but concealed its combative purpose through music, song, and dance.
 
-        Even after the abolition of slavery in Brazil in 1888, capoeira was illegal and its practitioners were socially ostracized for more than 40 years. Mestre Bimba, the legendary Capoeira Master, rescued the art form and proved its legitimacy, opening capoeira’s first official school in Bahia, Brazil.
+        Even after the abolition of slavery in Brazil in 1888, capoeira was illegal and its practitioners were 
+        socially ostracized for more than 40 years. Mestre Bimba, the legendary Capoeira Master, rescued the art form 
+        and proved its legitimacy, opening capoeira’s first official school in Bahia, Brazil.
 
-        Capoeira was born as an expression of resistance and resilience that brought spiritual and emotional empowerment. The cultures of enslaved Africans, Brazilian indigenous peoples, and Portuguese immigrants all contributed to the art of capoeira, and the art form is a reflection of the cultural and social integration of the diverse people comprising modern-day Brazil.
+        Capoeira was born as an expression of resistance and resilience that brought spiritual and emotional empowerment. 
+        The cultures of enslaved Africans, Brazilian indigenous peoples, and Portuguese immigrants all contributed 
+        to the art of capoeira, and the art form is a reflection of the cultural and social integration of the diverse 
+        people comprising modern-day Brazil.
 
-        Capoeira has developed into a means of empowerment and a forum for social and cultural exchange. It is now an internationally respected art of grace and strength that combines ritual, self-defense, acrobatics, and music.'
+        Capoeira has developed into a means of empowerment and a forum for social and cultural exchange. 
+        It is now an internationally respected art of grace and strength that combines ritual, self-defense, acrobatics,
+         and music.'
 
         source: Abadá Capoeira (https://www.abada.org/capoeira-history/)
         """)
@@ -43,12 +54,32 @@ with tab3:
         st.write("""
         Birth name: Manuel dos Reis Machado (November 23, 1900 – February 5, 1974)
         
-        Mestre Bimba is known as the founder of one of the most practised styles of Capoeira: capoeira regional. He is credited with being a major contributor to the legalization of Capoeira in Brazil in 1937, and in that same year officially registered the nation's first Capoeira center.
+        Mestre Bimba is known as the founder of one of the most practised styles of Capoeira: capoeira regional, 
+        a style which modernized capoeira by incorporating more martial techniques and making it more effective. 
+        He is credited with being a major contributor to the legalization of Capoeira in Brazil in 1937, 
+        and in that same year officially registered the nation's first Capoeira center.
+        Bimba was born on November 23, 1900, in Salvador, Bahia, Brazil. He began learning capoeira at age 12 
+        and later developed his own style to counter the perception of capoeira as merely a dance or ritual. 
+        He introduced new kicks and movements, and emphasized teaching capoeira to a broader audience, 
+        which helped in its decriminalization. Bimba was also an accomplished singer and berimbau player. 
+        He passed away on February 5, 1974, leaving a lasting legacy in the world of capoeira.
+        
+        source: Wikipedia (https://en.wikipedia.org/wiki/Manuel_dos_Reis_Machado), 
+        summarized with help of Microsoft Copilot
         """)
     with st.expander("About Mestre Pastinha"):
         st.image("https://upload.wikimedia.org/wikipedia/en/0/0d/Mestre_Pastinha.jpg",
                  width=300,
                  caption="This file is licensed under the Creative Commons Attribution 3.0 Unported license.")
         st.write("""
+        Birth name: Vicente Ferreira Pastinha (April 5, 1889 - November 13, 1981)
         
+        Mestre Pastinha was born on April 5, 1889, in Salvador, Bahia, Brazil. He is celebrated for his dedication to 
+        preserving the traditional style of capoeira known as Capoeira Angola. Unlike the more modern Capoeira Regional,
+         Capoeira Angola emphasizes the playful and ritualistic elements of the martial art. 
+        Pastinha opened the first official Capoeira Angola academy, called the Centro Esportivo de Capoeira Angola, 
+        in 1942. His teachings focused on the philosophy, history, and techniques of capoeira, ensuring that its 
+        African roots were honored. He passed away on November 13, 1981.
+        
+        source: Wikipedia (https://de.wikipedia.org/wiki/Mestre_Pastinha), summarized with help of Microsoft Copilot
         """)
